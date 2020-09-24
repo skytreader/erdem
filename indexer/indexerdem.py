bf3abdd007b24d95592ab74cbe86976b78fe7d47
Chad Estioco <chadestioco@gmail.com>
2020-09-24 13:04:39 +0800

    Index into the files table.

--- indexer/indexerdem.py
+++ indexer/indexerdem.py
@@ -11,8 +11,9 @@ class Indexerdem(object):
         cursor = self.conn.cursor()
 
         cursor.execute("PRAGMA foreign_keys=ON")
+        # FIXME what if tables already exist?
         cursor.execute("""CREATE TABLE files
-                          (id INTEGER PRIMARY KEY ASC, filename TEXT);""")
+                          (id INTEGER PRIMARY KEY ASC, filename TEXT UNIQUE NOT NULL);""")
         cursor.execute("""CREATE TABLE persons
                           (id INTEGER PRIMARY KEY ASC, firstname TEXT NOT NULL, lastname TEXT);""")
         cursor.execute("""CREATE TABLE participation
@@ -21,7 +22,18 @@ class Indexerdem(object):
                            FOREIGN KEY(file_id) REFERENCES file(id))""")
         self.conn.commit()
 
+    def __normalize_filename(self, filename: str) -> str:
+        spam = filename.split(".")
+        # TODO Check, there might be dots before the extension too!
+        return spam[0]
+
+    def index(self, filename: str) -> None:
+        cleaned = self.__normalize_filename(filename)
+        cursor = self.conn.cursor()
+        cursor.execute("INSERT INTO files (filename) VALUES (?)", (filename,))
+
 if __name__ == "__main__":
     parser = ArgumentParser(description="indexer for erdem.")
     indexer: Indexerdem = Indexerdem("cache.db")
     indexer.init()
+    indexer.index("lin.mp4")
