693a8b034e6e5852c6ba3c7ac5c0f6dca530654e
Chad Estioco <chadestioco@gmail.com>
2020-10-17 13:02:13 +0800

    sqlite3 showoff.

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import sqlite3 from 'sqlite3';
 
+const db = new sqlite3.Database("cache.db");
 const app: express.Application = express();
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
+    db.each("SELECT * FROM files;", (err, row) => {
+        console.log(row.filename);
+    });
 });
 
 app.listen(16981, () => {
