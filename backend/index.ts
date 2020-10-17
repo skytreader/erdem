3df72b8baf3b3ea3c201595973f57b894d4f3c14
Chad Estioco <chadestioco@gmail.com>
2020-10-18 03:19:14 +0800

    Verflixt nochmal nodejs! Verflixt nochmal!

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import sqlite3 from 'sqlite3';
+import {promisify} from 'util';
 
 const db = new sqlite3.Database("cache.db");
+
+const aDbAll = promisify(db.all).bind(db);
+
 const app: express.Application = express();
 
 app.get("/", (req, res) => {
@@ -9,6 +13,11 @@ app.get("/", (req, res) => {
     db.each("SELECT * FROM files;", (err, row) => {
         console.log(row.filename);
     });
+});
+
+app.get("/files", async (req, res) => {
+    const rows = await aDbAll("SELECT * FROM files;");
+    return res.json(rows);
 });
 
 app.listen(16981, () => {
