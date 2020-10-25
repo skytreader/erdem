bea6c59c6b96d5f453f1544b0e603bf9717cd8d5
Chad Estioco <chadestioco@gmail.com>
2020-10-25 15:20:31 +0800

    Add docs.

---
+++ backend/readme.markdown
@@ -0,0 +1,5 @@
+# erdem backend
+
+To compile: `npm run tsc`. This will transpile `index.ts` to `index.js`.
+
+To run: `node index.js` assuming built from step above.
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,10 @@
+/*
+IMPORTANT: Do not change this file's extension to ts(x) so long as Arwes does
+not have type definitions available.
+
+For some reason, `npm run build` processes this just fine with a .js extension
+(and no Arwes typedefs) but not if you change it to ts(x).
+*/
 import React from 'react';
 import {ThemeProvider, createTheme, Arwes} from 'arwes';
 
