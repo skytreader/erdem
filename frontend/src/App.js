7bfaf84cdf252483f262c303c34f30704dc1ee34
Chad Estioco <chadestioco@gmail.com>
2020-10-26 04:44:36 +0800

    Rewrite using more traditional class Component structure.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,26 @@ not have type definitions available.
 For some reason, `npm run build` processes this just fine with a .js extension
 (and no Arwes typedefs) but not if you change it to ts(x).
 */
-import React from 'react';
+import React, {useState} from 'react';
 import logo from "./img/erdem-logo.png";
 import {ThemeProvider, createTheme, Arwes, Row, Col} from 'arwes';
 
-const App = () => (
-    <ThemeProvider theme={createTheme()}>
-      <Arwes animate show>
-        <Row>
-          <Col s={0} m={3}></Col>
-          <Col s={12} m={6}><h1><img src={logo} alt="Erdem Logo" />Erdem</h1></Col>
-          <Col s={0} m={3}></Col>
-        </Row>
-      </Arwes>
-    </ThemeProvider>
-)
+class Erdem extends React.Component {
 
-export default App;
+    render() {
+        return (
+            <ThemeProvider theme={createTheme()}>
+              <Arwes animate show>
+                <Row>
+                  <Col s={0} m={3}></Col>
+                  <Col s={12} m={6}><h1><img src={logo} alt="Erdem Logo" />Erdem</h1></Col>
+                  <Col s={0} m={3}></Col>
+                </Row>
+              </Arwes>
+            </ThemeProvider>
+        )
+    }
+}
+
+
+export default Erdem;
