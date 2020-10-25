c7af755d911a76f88f08636cc1c6ce9b75672b3f
Chad Estioco <chadestioco@gmail.com>
2020-10-26 05:39:31 +0800

    Full loop HTTP request.
    Whowouldathunk it'd be this hard? Also, it is not yet rendering properly.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,44 @@ not have type definitions available.
 For some reason, `npm run build` processes this just fine with a .js extension
 (and no Arwes typedefs) but not if you change it to ts(x).
 */
-import React, {useState} from 'react';
+import React from 'react';
 import logo from "./img/erdem-logo.png";
 import {ThemeProvider, createTheme, Arwes, Row, Col} from 'arwes';
 
 class Erdem extends React.Component {
     
+    constructor(props) {
+        super(props);
+        this.state = {
+            mediaItems: []
+        };
+    }
+
     componentDidMount() {
         document.title = "Erdem";
+        fetch("http://localhost:16981/fetch/files")
+            .then(res => res.json())
+            .then((result) => {
+                console.log(result);
+                this.setState({
+                    mediaItems: result
+                });
+            },
+            (error) => {
+                console.error("Error occurred", error);
+        });
     }
 
     render() {
+        console.log("wat");
+        const mediaItemsRender = this.state.mediaItems.map((file) => (
+            <Row key={file.id}>
+                <Col s={0} m={3}></Col>
+                <Col s={12} m={6}>{file.name}</Col>
+                <Col s={0} m={3}></Col>
+            </Row>
+        ))
+        console.log(mediaItemsRender);
         return (
             <ThemeProvider theme={createTheme()}>
               <Arwes animate show>
@@ -24,6 +51,7 @@ class Erdem extends React.Component {
                   <Col s={12} m={6}><h1><img src={logo} alt="Erdem Logo" />Erdem</h1></Col>
                   <Col s={0} m={3}></Col>
                 </Row>
+                {mediaItemsRender}
               </Arwes>
             </ThemeProvider>
         )
