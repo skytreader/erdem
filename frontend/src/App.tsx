777cc2512a6a7008fff0a989ab4e61fc209173af
Chad Estioco <chadestioco@gmail.com>
2020-12-20 00:26:31 +0800

    Refactor FileList.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./img/erdem-logo.png";
 import {ThemeProvider, createTheme, Arwes, Row, Col} from "arwes";
 import {BrowserRouter, Switch, Route, Link, withRouter} from "react-router-dom";
+import FileList from "./FileList";
 
 import {erdemCentered} from "./utils";
 
@@ -36,72 +37,6 @@ class Erdem extends React.Component {
               </ThemeProvider>
             </BrowserRouter>
         )
-    }
-}
-
-interface MediaItem {
-    filename: string;
-    id: number;
-}
-
-interface FileListState {
-    mediaItems: MediaItem[];
-    isError: boolean;
-}
-
-class FileList extends React.Component<any, FileListState> {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            mediaItems: [],
-            isError: false
-        };
-    }
-
-    componentDidMount() {
-        fetch("http://localhost:16981/fetch/files")
-            .then(res => res.json())
-            .then((result) => {
-                this.setState({
-                    mediaItems: result.sort((a: MediaItem, b: MediaItem) => {
-                        var normA = a.filename.toUpperCase();
-                        var normB = b.filename.toUpperCase();
-
-                        if (normA < normB) {
-                            return -1;
-                        } else if (normA > normB) {
-                            return 1;
-                        } else {
-                            return 0;
-                        }
-                    }),
-                    isError: false
-                });
-            },
-            (error) => {
-                this.setState({
-                    mediaItems: [],
-                    isError: true
-                });
-                console.error("Error occurred", error);
-        });
-    }
-
-    render() {
-        if (this.state.isError) {
-            // TODO Style better.
-            return (
-                <div>Error fetching files. Check server.</div>
-            )
-        } else {
-            return this.state.mediaItems.map((file) => (
-                <Row key={file.id}>
-                    <Col s={0} m={3}></Col>
-                    <Col s={12} m={6}><Link to={`/participants/${file.id}`}>{file.filename}</Link></Col>
-                    <Col s={0} m={3}></Col>
-                </Row>
-            ));
-        }
     }
 }
 
---
+++ frontend/src/FileList.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import {Row, Col} from "arwes";
+import {Link} from "react-router-dom";
+
+interface MediaItem {
+    filename: string;
+    id: number;
+}
+
+interface FileListState {
+    mediaItems: MediaItem[];
+    isError: boolean;
+}
+
+class FileList extends React.Component<any, FileListState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            mediaItems: [],
+            isError: false
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:16981/fetch/files")
+            .then(res => res.json())
+            .then((result) => {
+                this.setState({
+                    mediaItems: result.sort((a: MediaItem, b: MediaItem) => {
+                        var normA = a.filename.toUpperCase();
+                        var normB = b.filename.toUpperCase();
+
+                        if (normA < normB) {
+                            return -1;
+                        } else if (normA > normB) {
+                            return 1;
+                        } else {
+                            return 0;
+                        }
+                    }),
+                    isError: false
+                });
+            },
+            (error) => {
+                this.setState({
+                    mediaItems: [],
+                    isError: true
+                });
+                console.error("Error occurred", error);
+        });
+    }
+
+    render() {
+        if (this.state.isError) {
+            // TODO Style better.
+            return (
+                <div>Error fetching files. Check server.</div>
+            )
+        } else {
+            return this.state.mediaItems.map((file) => (
+                <Row key={file.id}>
+                    <Col s={0} m={3}></Col>
+                    <Col s={12} m={6}><Link to={`/participants/${file.id}`}>{file.filename}</Link></Col>
+                    <Col s={0} m={3}></Col>
+                </Row>
+            ));
+        }
+    }
+}
+
+export default FileList;
