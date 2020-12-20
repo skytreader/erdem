74720f21ee855c6455a6b81eff34085eb34be563
Chad Estioco <chadestioco@gmail.com>
2020-12-21 01:18:14 +0800

    Finally make that switching effect!

--- frontend/src/ListToggle.tsx
+++ frontend/src/ListToggle.tsx
@@ -24,8 +24,8 @@ class ListToggle extends React.Component<any, ListToggleState> {
         const page = [
             (
                 <Row>
-                    <Col s={6} m={3} offset={["s0", "m3"]}><Button className="tab">Files</Button></Col>
-                    <Col s={6} m={3}><Button className="tab">Performers</Button></Col>
+                    <Col s={6} m={3} offset={["s0", "m3"]}><Button className="tab" onClick={() => this.toggle("performers")}>Performers</Button></Col>
+                    <Col s={6} m={3}><Button className="tab" onClick={() => this.toggle("files")}>Files</Button></Col>
                 </Row>
             ),
         ]
