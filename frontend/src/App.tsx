678c7082a8cc7aeb918c8392a598acbbc467612b
Chad Estioco <chadestioco@gmail.com>
2021-01-10 01:31:06 +0800

    Fix react references.
    Can't believe how hard it is to get TypeScript to just play along with this one.
    Look at all that dirty redundant null checks (even had to make an assertion!).

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,11 +9,13 @@ import PerformanceList from "./PerformanceList";
 import {SearchResults} from "./FileList";
 
 class Erdem extends React.Component<any, any> {
+    // TS/JS is dumb. You can't put this in the constructor. Because apparently
+    // some of these methods might be called before the object is constructed/initialized.
+    private searchQuery: React.RefObject<HTMLInputElement> = React.createRef<HTMLInputElement>();
     
     constructor(props: any) {
         super(props);
         this.state = {
-            searchQuery: ""
         };
 
         this.handleQueryTyped = this.handleQueryTyped.bind(this);
@@ -27,9 +29,20 @@ class Erdem extends React.Component<any, any> {
         this.setState({searchQuery: event.target.value});
     }
 
+    decideSQVal(): string {
+        // Okay TS is dumb, unlike MyPy. That's why you need all this bloody
+        // bullshit type checks _and_ not null assertions! WTF!
+        const tsDumber = this.searchQuery;
+        if (tsDumber != null){
+            return tsDumber!.current!.value;
+        } else {
+            return "";
+        }
+    }
+
     render() {
         return (
-            <BrowserRouter>
+            <BrowserRouter forceRefresh={true}>
               <ThemeProvider theme={createTheme()}>
                 <Arwes animate show>
                   <Row>
@@ -39,10 +52,10 @@ class Erdem extends React.Component<any, any> {
                   <Row>
                     <Col s={0} m={3}></Col>
                     <Col s={6} m={5}>
-                      <input className="fullwidth" type="text" placeholder="Query" value={this.state.searchQuery} onChange={this.handleQueryTyped}/>
+                      <input className="fullwidth" type="text" placeholder="Query" value={this.state.searchQuery} ref={this.searchQuery}/>
                     </Col>
                     <Col s={0} m={1}>
-                      <Button className="fullwidth" onClick={() => {erdemHistory.push("/search/" + this.state.searchQuery)}}>
+                      <Button className="fullwidth" onClick={() => {erdemHistory.push("/search/" + this.decideSQVal()) }}>
                       Search
                       </Button>
                     </Col>
