379c1e48a5599ba670d36e069f250f5fb6c3ae08
Chad Estioco <chadestioco@gmail.com>
2026-01-28 19:03:15 +0100

    Mockup.

--- indexer/tui.py
+++ indexer/tui.py
@@ -1,10 +1,46 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Input
+from textual.containers import HorizontalGroup
+from textual.widgets import Button, Header, Input, Label, ListView, ListItem, TabbedContent, TabPane
+
+class ErdemHeader(Header):
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+
+class ErdemSearch(HorizontalGroup):
+
+    def compose(self) -> ComposeResult:
+        search_box = Input(placeholder="Search by title, tag, or performer", id="search-box")
+        search_box.styles.width = "93%"
+        yield search_box
+        search_btn = Button("Search", id="search")
+        yield search_btn
 
 class ErdemHomeApp(App):
 
     def compose(self) -> ComposeResult:
-        yield Input(placeholder="Enter a title, tag, or performer", id="search-box")
+        yield Header()
+        yield ErdemSearch()
+        with TabbedContent(initial="media-list"):
+            with TabPane("Media", id="media-list"):
+                yield ListView(
+                    ListItem(Label("Casino Royale")),
+                    ListItem(Label("Quantum of Solace")),
+                    ListItem(Label("Skyfall")),
+                    ListItem(Label("Spectre")),
+                    ListItem(Label("No Time to Die"))
+                )
+            with TabPane("Performers", id="performers-list"):
+                yield ListView(
+                    ListItem(Label("Craig, Daniel")),
+                    ListItem(Label("de Aramas, Ana")),
+                    ListItem(Label("Green, Eva")),
+                    ListItem(Label("Mikkelsen, Mads"))
+                )
+
+    def on_mount(self) -> None:
+        self.title = "Erdem"
+        self.sub_title = "Media Notes"
 
 if __name__ == "__main__":
     app = ErdemHomeApp()
